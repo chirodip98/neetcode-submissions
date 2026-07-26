bc3511cec85778c58f005a1d079382b80378d992
Chirodip Lodh Choudhury <52487382+chirodip98@users.noreply.github.com>
2026-07-26 11:59:49 +0530

    Update: balanced-binary-tree - submission-1

--- Data Structures & Algorithms/balanced-binary-tree/submission-1.java
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.java
@@ -15,21 +15,20 @@
  */
 
 class Solution {
-    public boolean isBalanced(TreeNode root) {
-        
-        if(root==null) return true;
-        int leftSubtree = heightOfTree(root.left);
-        int rightSubtree = heightOfTree(root.right);
-        return Math.abs(rightSubtree - leftSubtree) <=1 && (isBalanced(root.left) && isBalanced(root.right));
 
-    }
-
-    public int heightOfTree(TreeNode root)
+    boolean isBalanced=true;
+    private int height(TreeNode root)
     {
-        if(root==null) return 0;
-        if(root.left == null && root.right == null) return 1;
-        int left = heightOfTree(root.left);
-        int right = heightOfTree(root.right);
+        if(root==null) return -1;
+        int left = height(root.left);
+        int right = height(root.right);
+        isBalanced  = isBalanced && Math.abs((left-right)) <=1;
         return 1 + Math.max(left, right);
+    }
+    public boolean isBalanced(TreeNode root) {
+
+        height(root);
+        return isBalanced;
+        
     }
 }
